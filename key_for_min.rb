cfd0f7990d9d6eaf9781f025ec3180bd4cc8582a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-24 15:31:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   name_hash.collect {|k, v| array << v}
   len = 0
   while len < array.size 
-    name_hash.detect {|k, v| 
-  if v == array.min
+    name_hash.each {|k, v| 
+      if v == array.min
     k
   end}
 end
